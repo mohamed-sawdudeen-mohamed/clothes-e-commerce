88e01e958117a71a820e37a38ed3384c6917ce64
MS Mohomad <sawdudeenm@gmail.com>
2023-01-07 16:05:00 +0530

    Announce and Navbar tasks

---
+++ src/components/Announce.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import { Close } from "@mui/icons-material";
+
+function Announce() {
+  const [announceStyle, setAnnounceStyle] = useState(
+    "bg-[#8a4af3] font-bold text-white flex items-center justify-center"
+  );
+
+  const handlClose = () => {
+    setAnnounceStyle(announceStyle + " hidden");
+  };
+  return (
+    <div className={announceStyle}>
+      <h2>Hurry up it's 40% off now</h2>
+      <Close className="cursor-pointer" onClick={handlClose} />
+    </div>
+  );
+}
+
+export default Announce;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Announce from "../components/Announce";
+import Navbar from "../components/Navbar";
+
+function Home() {
+  return (
+    <div>
+      <Announce />
+      <Navbar />
+    </div>
+  );
+}
+
+export default Home;
